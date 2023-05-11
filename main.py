1a413b74b801488ede47d914e8dc56d455f6e56f
Gabriel da Silva <91972934+gasilvabrielcode@users.noreply.github.com>
2023-05-11 13:01:34 -0300

    light commit

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request, session
-from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper,\
-    ServiceContext
+from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
 from langchain import OpenAI
 import os
 
-
 def construct_index(directory_path):
     max_input_size = 4096
-    num_outputs = 2000
+    num_outputs = 1000  # Reduz o número de tokens de saída
     max_chunk_overlap = 20
     chunk_size_limit = 600
 
@@ -24,19 +22,15 @@ def construct_index(directory_path):
 
     return index
 
-
 def ask_ai(query):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
     response = index.query(query)
     response = str(response)
     return response
 
-
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
-
 app.config['SESSION_TYPE'] = 'filesystem'
-
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -46,9 +40,10 @@ def home():
     if request.method == 'POST':
         user_query = request.form['user_query']
         response = ask_ai(user_query)
-        with open('log.txt', 'a') as f:
-            f.writelines(user_query + '\n' + response + '\n' + '\n')
-            f.close()
+
+        if len(session['messages']) >= 5:  # Mantém apenas as últimas 5 mensagens
+            session['messages'] = session['messages'][-4:]
+
         session['messages'].append({'user_query': user_query, 'response': response})
         session.modified = True  # Garante que a sessão seja salva após a modificação
         return render_template('index.html', last_message=session['messages'][-1], messages=session['messages'])
@@ -58,7 +53,7 @@ def home():
     return render_template('index.html', last_message=None, messages=session['messages'])
 
 
-
+os.environ["OPENAI_API_KEY"] = "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"
 construct_index("data")
 
 if __name__ == "__main__":
